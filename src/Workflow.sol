49320d850096ac986d89db7458321eb83abbba74
Aroon4020 <kessaniaroon4020@gmail.com>
2024-08-09 16:45:06 -0700

    feat: isActiveWorkflow

--- src/Workflow.sol
+++ src/Workflow.sol
@@ -1,24 +1,44 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.23;
 
-import {IProtocolFees} from "./IProtocolFees.sol";
-import {IDittoEntryPoint} from "./IDEP.sol";
+import {IProtocolFees} from "./interfaces/IProtocolFees.sol";
+import {IDittoEntryPoint} from "./interfaces/IDEP.sol";
 
+/**
+ * @title Workflow
+ * @dev This contract manages workflow registrations and associated gas limits and prices.
+ * It integrates with Ditto Entry Point (DEP) and a protocol fees contract.
+ */
 contract Workflow {
     IDittoEntryPoint constant DEP =
         IDittoEntryPoint(0x5FD0026a449eeA51Bd1471E4ee8df8607aaECC24);
     IProtocolFees constant protocolFees =
         IProtocolFees(0xCFE38b65b0cCDD6b78e578d8E07891EFEE51a655);
+
     struct TxData {
         uint256 maxGasPrice;
         uint256 maxGasLimit;
         bool isExecuted;
     }
+
+    /// @notice Mapping to store transaction data by workflow key
     mapping(uint256 => TxData) public txData;
+
+    /// @notice Mapping to store block details by block number
     mapping(uint256 => uint256) public blockDetails;
 
+    /**
+     * @notice Fallback function to receive Ether.
+     */
     receive() external payable {}
 
+    /**
+     * @notice Activates a workflow by registering it with the DEP and setting its gas data.
+     * @param workflowKey The unique key for the workflow.
+     * @param maxGasPrice The maximum gas price that can be used for transactions.
+     * @param maxGasLimit The maximum gas limit that can be used for transactions.
+     * @dev Reverts if the workflow key has already been registered.
+     */
     function activate(
         uint256 workflowKey,
         uint256 maxGasPrice,
@@ -30,6 +50,12 @@ contract Workflow {
         txData[workflowKey] = TxData(maxGasPrice, maxGasLimit, false);
     }
 
+    /**
+     * @notice Pays the prefund for a workflow, which is calculated based on gas price and limits.
+     * @param workflowKey The unique key for the workflow.
+     * @return The maximum gas limit for the workflow.
+     * @dev Reverts if the caller is not the DEP or if the transaction fails.
+     */
     function payPrefund(
         uint256 workflowKey
     ) external virtual returns (uint256) {
@@ -46,12 +72,32 @@ contract Workflow {
         return maxGasLimit;
     }
 
+    /**
+     * @notice Runs the workflow using the DEP.
+     * @param workflowKey The unique key for the workflow.
+     * @dev Calls the internal `_run` function to execute the workflow.
+     */
     function runFromDEP(uint256 workflowKey) external {
         _run(workflowKey);
     }
 
+    /**
+     * @notice Checks if a workflow is active.
+     * @param workflowKey The unique key for the workflow.
+     * @return Returns true if the workflow is active (i.e., not executed).
+     */
+    function isActiveWorkflow(
+        uint256 workflowKey
+    ) external view returns (bool) {
+        return !txData[workflowKey].isExecuted;
+    }
+
+    /**
+     * @notice Internal function to execute a workflow.
+     * @param workflowKey The unique key for the workflow.
+     * @dev Records the block timestamp and marks the workflow as executed.
+     */
     function _run(uint256 workflowKey) internal {
-        require(txData[workflowKey].isExecuted == false, "Already executed");
         blockDetails[block.number] = block.timestamp;
         txData[workflowKey].isExecuted = true;
     }
--- test/Workflow.t.sol
+++ test/Workflow.t.sol
@@ -2,9 +2,10 @@
 pragma solidity 0.8.23;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "../src/Workflow.sol";
-import {IProtocolFees} from "../src/IProtocolFees.sol";
-import {IDittoEntryPoint} from "../src/IDEP.sol";
+import {IProtocolFees} from "../src/interfaces/IProtocolFees.sol";
+import {IDittoEntryPoint} from "../src/interfaces/IDEP.sol";
 
 contract WorkflowTest is Test {
     Workflow public workflow;
@@ -18,29 +19,32 @@ contract WorkflowTest is Test {
         vm.deal(address(workflow), 100 ether);
     }
 
+    receive() external payable {}
+
     function testActivate() public {
         uint256 workflowKey = 1;
         uint256 maxGasPrice = 50 gwei;
         uint256 maxGasLimit = 500000;
         workflow.activate(workflowKey, maxGasPrice, maxGasLimit);
-        (uint256 storedGasPrice, uint256 storedGasLimit, bool isexe) = workflow
-            .txData(workflowKey);
+        (
+            uint256 storedGasPrice,
+            uint256 storedGasLimit,
+            bool isExecuted
+        ) = workflow.txData(workflowKey);
+
         assertEq(storedGasPrice, maxGasPrice, "Max gas price mismatch");
         assertEq(storedGasLimit, maxGasLimit, "Max gas limit mismatch");
-        assertEq(isexe, false);
+        assertEq(isExecuted, false);
     }
 
     function testPayPrefund() public {
         uint256 workflowKey = 1;
         uint256 maxGasPrice = 50 gwei;
         uint256 maxGasLimit = 500000;
-
-        vm.startPrank(address(DEP));
         workflow.activate(workflowKey, maxGasPrice, maxGasLimit);
-
+        vm.startPrank(address(DEP));
         vm.txGasPrice(45 gwei);
         uint256 gasLimit = workflow.payPrefund(workflowKey);
-
         assertEq(gasLimit, maxGasLimit, "Gas limit mismatch");
     }
 
@@ -49,12 +53,15 @@ contract WorkflowTest is Test {
         uint256 maxGasPrice = 50 gwei;
         uint256 maxGasLimit = 100000;
         workflow.activate(workflowKey, maxGasPrice, maxGasLimit);
+
         vm.roll(100);
         vm.warp(1000);
+
         vm.startPrank(address(DEP));
+        assertTrue(workflow.isActiveWorkflow(workflowKey));
         workflow.payPrefund(workflowKey);
         workflow.runFromDEP(workflowKey);
         assertEq(workflow.blockDetails(100), 1000);
     }
-    receive() external payable {}
+    
 }
